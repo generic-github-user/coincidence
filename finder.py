e88a4f16ac65c39b6eb8bbf83c19d1762d0463ba
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2021-06-26 00:53:13 -0400

    Add list of simple functions
    These can apply to either individual values or arrays

--- finder.py
+++ finder.py
@@ -5,3 +5,13 @@ import string
 import numpy as np
 import zlib
 import random
+possible_functions = [
+    [lambda x: x**2, 'square'],
+    [lambda x: x**3, 'cube'],
+    # [lambda x: x**4, 'x ^ 4'],
+    # [lambda x: 2**x, '2 ^ x'],
+    # np.sin,
+    # np.cos,
+    # np.tan,
+    # np.round
+]
