08b88187cdca591c71113001201655b9b5b296fc
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2021-06-26 23:24:53 -0400

    Add list of hand-selected criteria that can be used to generate a value from a string (or numeric representation of its characters)

--- finder.py
+++ finder.py
@@ -20,3 +20,18 @@ reducing_functions = [
     np.product,
     # np.mean,
 ]
+criteria = [
+    lambda x: np.sum(x),
+    lambda x: np.product(x),
+    lambda x: np.mean(x),
+    lambda x: np.sum(np.array(x)**2),
+    lambda x: np.sum(np.array(x)**3),
+    lambda x: np.product(x**2),
+    lambda x: np.product(x**3),
+    lambda x: np.sum((x**2)+(x**2)),
+    lambda x: np.sum((x**2)*(x**2)),
+    lambda x: np.sum(np.sin(x)*(x**2)),
+    lambda x: np.sum(x*np.arange(1, x.size+1, 1)),
+    lambda x: np.sum(x*np.flip(x)),
+    lambda x: np.sum(2**x),
+]
